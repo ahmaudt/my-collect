5e45da35db501cd0f6f9cff9085a9b9ddfa3fa4f
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-21 16:16:29 -0500

    commit tree 67c374bd4161ea9e819c88c7ed35598cfd90304e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+    if block_given?
+        counter = 0
+        collection = []
+        while counter < array.length
+            collection << yield(array[counter])
+            counter+=1
+        end
+        collection
+    else 
+        puts "Block not given!"
+    end
+end
 
+languages = ["ruby","javascript","python","objective-c"]
+my_collect(languages) do |lang|
+    lang.upcase
+end
 
